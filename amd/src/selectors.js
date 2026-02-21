166eeced9b47001fd3c7a62ff1eced0884c7caeb
wilber-buendata <wilber@buendata.com>
2026-02-21 16:29:28 -0500

    feat: add DOM selectors module for AI course creation streaming
    Add new selectors.js module with data-region selectors for root, status, and output elements used in AI course creation streaming interface. Include minified build files and source maps.

---
+++ amd/src/selectors.js
@@ -0,0 +1,28 @@
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * DOM selectors for AI course creation streaming page.
+ *
+ * @module     local_coursegen/selectors
+ * @copyright  2025 Wilber Narvaez <https://datacurso.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+export const regions = {
+    root: "[data-region='local_coursegen/aicoursecreation/root']",
+    status: "[data-region='local_coursegen/aicoursecreation/status']",
+    output: "[data-region='local_coursegen/aicoursecreation/output']",
+};
