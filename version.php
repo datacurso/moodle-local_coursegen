e252aa4d21755e623ce32f1653da4af4a7ba31a8
wilber-buendata <wilber@buendata.com>
2026-06-02 11:30:18 -0500

    chore: bump version for course review panel feature

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_coursegen';
 $plugin->release = '1.5.0';
-$plugin->version = 2026060101;
+$plugin->version = 2026060201;
 $plugin->requires = 2024100700; // Moodle 4.5.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->supported = [405, 501];
