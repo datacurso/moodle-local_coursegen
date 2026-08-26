c50e038edfd146dc0e113a9d8f65c71aa6c05075
wilber-buendata <wilber@buendata.com>
2026-08-26 14:22:46 -0500

    chore(local_coursegen): bump version to 2.0.4

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_coursegen';
-$plugin->release = '2.0.3';
-$plugin->version = 2026082409;
+$plugin->release = '2.0.4';
+$plugin->version = 2026082600;
 $plugin->requires = 2024100700; // Moodle 4.5.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->supported = [405, 501];
